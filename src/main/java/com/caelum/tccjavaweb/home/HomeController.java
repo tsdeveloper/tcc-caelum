3cc150f09048a28acb5a8839efb23f8b81bf1d4d
Web Analyst - FM <Web Analyst - FM@DESKTOP-F9MPJ44>
2018-02-20 12:29:59 -0300

    Add layout page

--- src/main/java/com/caelum/tccjavaweb/home/HomeController.java
+++ src/main/java/com/caelum/tccjavaweb/home/HomeController.java
@@ -16,6 +16,6 @@ class HomeController {
 
     @GetMapping("/")
     String index(Principal principal) {
-        return principal != null ? "home/homeSignedIn" : "home/homeNotSignedIn";
+        return principal != null ? "home/homeSignedIn" : "home/index";
     }
 }
